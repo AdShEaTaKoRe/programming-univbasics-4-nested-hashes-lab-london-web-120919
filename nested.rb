bc9e499c1112d4fd8c89bde2f2bb316cc246d37d
Tommy Berman <tomy.berman1@gmail.com>
2019-12-03 11:54:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,7 +77,9 @@ def adding_matz
         }
      }
      
-   programmer_hash[:yukihiro_matsumoto] = {}
+   programmer_hash[:yukihiro_matsumoto] = {
+     
+   }
    
    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
    programmer_hash[:yukihiro_matsumoto][:languages] = Array.new
